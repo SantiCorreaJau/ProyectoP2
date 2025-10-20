b152632ec052cdb0dd653c91c89ddfc0fbf2c8fb
Juan Baldenegro <132092063+juanBalde2002@users.noreply.github.com>
2025-10-20 09:02:51 -0300

    .

--- Proyecto/src/IUsuario.cs
+++ Proyecto/src/IUsuario.cs
@@ -1,4 +1,4 @@
-﻿namespace DefaultNamespace;
+﻿namespace Proyecto;
 
 public interface IUsuario
 {
--- Proyecto/src/Vendedor.cs
+++ Proyecto/src/Vendedor.cs
@@ -1,15 +1,20 @@
-﻿namespace DefaultNamespace;
+﻿namespace Proyecto;
 
-public class Vendedor
+public class Vendedor : IUsuario
 {
+    public string id { get; set; }
+    public string nombre { get; set; }
+    public string apellido { get; set; }
+    public string email { get; set; }
+    public string telefono { get; set; }
     public bool activo { get; set; }
 
     public Vendedor(string unId, string unNombre, string unApellido, string unTelefono, string unMail)
     {
-        this.id = unId;
-        this.nombre = unNombre;
-        this.apellido = unApellido;
-        this.telefono = unTelefono;
-        this.email = unEmail;
+        id = unId;
+        nombre = unNombre;
+        apellido = unApellido;
+        telefono = unTelefono;
+        email = unMail;
     }
 }
