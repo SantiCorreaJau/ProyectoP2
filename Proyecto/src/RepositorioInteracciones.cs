b4a881f2a859fdbc53a21fd3d8a5b674b12c4926
joaco <joaquin.garciaq@correo.ucu.edu.uy>
2025-10-20 12:06:51 -0300

    a

---
+++ Proyecto/src/RepositorioInteracciones.cs
@@ -0,0 +1,14 @@
+﻿namespace DefaultNamespace;
+
+public class RepositorioInteracciones
+{
+    public List<Interaccion> RepoInteracciones { get; set; }
+
+    public RepositorioInteracciones()
+    {
+        RepoInteracciones = new List<Interaccion>();
+    }
+    
+    public List<Interacciones> Filtrar
+    
+}
