b003c855f9279d379582db5c12e6225394ca1cf1
joaco <joaquin.garciaq@correo.ucu.edu.uy>
2025-10-20 09:46:15 -0300

    a

---
+++ Proyecto/src/Comentario.cs
@@ -0,0 +1,40 @@
+﻿namespace Proyecto;
+
+public class Comentario
+{
+    public class Comentario
+    {
+        public string id { get; set; }
+        public string texto { get; set; }
+        public string creadoEn { get; set; }
+        public string autorId { get; set; }
+        public string interaccionId { get; set; }
+        public bool visible { get; set; }
+
+        public Comentario(string id, string texto, string creadoEn, string autorId, string interaccionId, bool visible)
+        {
+            this.id = id;
+            this.texto = texto;
+            this.creadoEn = creadoEn;
+            this.autorId = autorId;
+            this.interaccionId = interaccionId;
+            this.visible = visible;
+        }
+
+        public void EditarTexto(string nuevoTexto)
+        {
+            texto = nuevoTexto;
+        }
+
+        public void Borrar()
+        {
+            visible = false;
+        }
+
+        public void Mostrar()
+        {
+            System.Console.WriteLine($"[{creadoEn}] {texto} (Autor: {autorId})");
+        }
+    }
+
+}
---
+++ Proyecto/src/Interaccion.cs
@@ -0,0 +1,58 @@
+﻿namespace DefaultNamespace;
+
+public interface Interaccion
+{
+    public class Interaccion
+    {
+        public string id { get; set; }
+        public string fechaHora { get; set; }
+        public string tema { get; set; }
+        public string direccion { get; set; }
+        public string estado { get; set; }
+        public string clienteId { get; set; }
+        public string vendedorId { get; set; }
+        public List<Comentario> comentarios { get; set; }
+
+        public Interaccion(string id, string fechaHora, string tema, string direccion, string estado, string clienteId, string vendedorId, List<Comentario> comentarios)
+        {
+            this.id = id;
+            this.fechaHora = fechaHora;
+            this.tema = tema;
+            this.direccion = direccion;
+            this.estado = estado;
+            this.clienteId = clienteId;
+            this.vendedorId = vendedorId;
+            this.comentarios = comentarios;
+        }
+
+        public void Registrar(string idCliente)
+        {
+            clienteId = idCliente;
+        }
+
+        public void CambiarEstado(string nuevoEstado)
+        {
+            estado = nuevoEstado;
+        }
+
+        public void AgregarComentario(string texto)
+        {
+            if (comentarios == null)
+            {
+                comentarios = new List<Comentario>();
+            }
+            string fechaActual = System.DateTime.Now.ToString();
+            comentarios.Add(new Comentario("", texto, fechaActual, vendedorId, id, true));
+        }
+
+        public bool EsPendiente()
+        {
+            return estado == "pendiente";
+        }
+
+        public bool EsReciente(string fechaLimite)
+        {
+            return string.Compare(fechaHora, fechaLimite) > 0;
+        }
+    }
+}
