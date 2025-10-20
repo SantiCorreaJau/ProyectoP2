22be056bb565190740ac9e9e9890e82eab7be00c
joaco <joaquin.garciaq@correo.ucu.edu.uy>
2025-10-20 10:30:22 -0300

    a

---
+++ Proyecto/src/Mensaje.cs
@@ -0,0 +1,20 @@
+﻿namespace Proyecto;
+
+public class Mensaje : Interaccion
+{
+    public string canal { get; set; }
+    public string contenido { get; set; }
+
+    public Mensaje(string id, string fechaHora, string tema, string direccion, string estado, string clienteId, string vendedorId, List<Comentario> comentarios, string canal, string contenido)
+        : base(id, fechaHora, tema, direccion, estado, clienteId, vendedorId, comentarios)
+    {
+        this.canal = canal;
+        this.contenido = contenido;
+    }
+
+    public override void Registrar(string idCliente)
+    {
+        clienteId = idCliente;
+        estado = "mensaje registrado";
+    }
+}
